1d680825b6f55b870feb155e896cf52e15164f8d
pctripsesp <pctripsesp@users.noreply.github.com>
2022-08-11 22:06:17 +0200

    Update payload_gen.py

--- payload_gen.py
+++ payload_gen.py
@@ -1,20 +1,24 @@
-import os
-import time
+## SET THIS VALUES
+first = 439
+last = 469
+file = 'download.sh'
 
 print('Log in please...')
 
-file = 'd.sh'
 with open (file, 'w') as download_file:
     download_file.write('#!/bin/bash\necho "Loggin please"\nsleep 15\nfirefox https://www.hackthebox.com/login\n\n')
+
 writeups = '' 
-for i in range(283, 469):
+
+for i in range(first, last+1):
     
     writeups += ' https://www.hackthebox.com/home/machines/writeup/'+str(i)
 
-    #os.system('chromium --no-sandbox '+ writeups +'&')
-    
     if (i % 10 == 0):
-        with open('d.sh', 'a') as download_file:
+        with open(file, 'a') as download_file:
             download_file.write('\nfirefox'+ writeups +'&\nsleep 20\n')
         
         writeups = ''
+
+with open(file, 'a') as download_file:
+    download_file.write('\nfirefox'+ writeups +'&\nsleep 20\n')
